a96e021d5d4ee9727622e653820c8ceb04338bd0
Dejan Despot <dejandespot7@gmail.com>
2020-01-28 11:53:22 +0100

    Test changes

--- frontend/src/Components/Login.test.js
+++ frontend/src/Components/Login.test.js
@@ -3,6 +3,7 @@ import { mount, configure } from "enzyme";
 import Login from "./Login";
 import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
+import { act } from "react-dom/test-utils";
 import "regenerator-runtime/runtime";
 
 it("renders text input with label for the email input", () => {
@@ -19,21 +20,13 @@ it("renders text input with label for the email input", () => {
   expect(emailInput.prop("id")).toEqual("email");
 });
 
-it("calls login prop function when form is submitted", async () => {
+it("calls login prop function when form is submitted, but does not pass validation", async () => {
   const login = jest.fn();
   const wrapper = mount(<Login login={login} />);
-  const emailInput = wrapper.find('input[name="email"]');
-
-  emailInput.instance().value = "test@example.com";
-
-  const passwordInput = wrapper.find('input[name="password"]');
-
-  passwordInput.instance().value = "pass123";
-
   const form = wrapper.find("form");
 
   /* fire events that update state */
   form.simulate("submit");
 
-  expect(login).toHaveBeenCalledTimes(1);
+  expect(login).toHaveBeenCalledTimes(0);
 });
